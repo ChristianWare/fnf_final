849f34d559cbfd2643fb746ebe55bd42bb13a09e
Chris Ware <c.p.ware@hotmail.com>
2025-06-22 20:56:13 -0700

    hero work

--- src/components/homePage/Hero/Hero.tsx
+++ src/components/homePage/Hero/Hero.tsx
@@ -1,11 +1,24 @@
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./Hero.module.css";
+import Button from "@/components/shared/Button/Button";
 
 export default function Hero() {
   return (
     <section className={styles.container}>
       <LayoutWrapper>
-        <h1 className={styles.heading}>We build interactive and immersive ecommerce websites</h1>
+        <div className={styles.content}>
+          <h1 className={styles.heading}>
+            We build <br /> <span className={styles.span}> blazing fast</span>{" "}
+            <br /> online stores
+          </h1>
+          <p className={styles.copy}>
+            Fonts & Footers builds lightning-fast, revenue-driven online stores
+            for small and medium-sized brands.
+          </p>
+          <div className={styles.btnContainer}>
+            <Button href="/" btnType="white" text="Submit a request" />
+          </div>
+        </div>
       </LayoutWrapper>
     </section>
   );
