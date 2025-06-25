8e4e36e37d9c1885bd8205c0e557bec117224873
Chris Ware <c.p.ware@hotmail.com>
2025-06-25 15:46:46 -0700

    team done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import AboutPageIntro from "@/components/aboutPage/AboutPageIntro/AboutPageIntro";
 import MissionandValues from "@/components/aboutPage/MissionandValues/MissionandValues";
+import OurTeam from "@/components/aboutPage/OurTeam/OurTeam";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutPageIntro />
       <MissionandValues />
+      <OurTeam />
     </main>
   );
 }
---
+++ src/components/aboutPage/OurTeam/OurTeam.tsx
@@ -0,0 +1,38 @@
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import styles from "./OurTeam.module.css";
+import SectionIntro from "@/components/shared/SectionIntro/SectionIntro";
+import Image from "next/image";
+import ChrisImg from "../../../../public/images/chris.png";
+
+export default function OurTeam() {
+  return (
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.sectionHeadingContainer}>
+              <SectionIntro title='Our Team' dotColor='blueDot' />
+            </div>
+            <h2 className={styles.heading}>
+              At its core, <span className={styles.span}>Fonts & Footers</span>{" "}
+              is just me <span className={styles.span}> — Chris — </span>your
+              dedicated full-stack developer and founder. With over eight years
+              of hands-on experience crafting high-performance Shopify and
+              Next.js storefronts, I work as an extension of your team to
+              deliver clean, conversion-focused sites.
+             
+            </h2>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.circleContainer}>
+              <div className={styles.pulsingCircles}></div>
+              <div className={styles.imgContainer}>
+                <Image src={ChrisImg} alt='' fill className={styles.img} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+}
