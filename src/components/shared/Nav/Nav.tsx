35786fe666d9a8583c17a03107e82b09067bc0a3
Chris Ware <c.p.ware@hotmail.com>
2025-06-25 14:28:44 -0700

    nav menu link href updated

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -7,11 +7,11 @@ import Button from "../Button/Button";
 import { useState } from "react";
 
 const navItems = [
-  { text: "About", href: "/#aboutus" },
-  { text: "Work", href: "/#work" },
-  { text: "Services", href: "/#services" },
-  { text: "faq", href: "/#faq" },
-  { text: "Contact", href: "/#contact" },
+  { text: "About", href: "/about" },
+  { text: "Work", href: "/work" },
+  { text: "Services", href: "/services" },
+  { text: "faq", href: "/faq" },
+  { text: "Contact", href: "/contact" },
 ];
 
 export default function Nav() {
@@ -22,53 +22,52 @@ export default function Nav() {
   };
 
   return (
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <div className={styles.logoContainer}>
+          <Logo
+            title='Fonts & Footers'
+            backgroundColor='backgroundColorWhite'
+            direction='center'
+          />
+        </div>
 
-      <header className={styles.header}>
-        <nav className={styles.navbar}>
-          <div className={styles.logoContainer}>
-            <Logo
-              title='Fonts & Footers'
-              backgroundColor='backgroundColorWhite'
-              direction='center'
-            />
-          </div>
+        <div
+          className={
+            isOpen === false
+              ? styles.navItems
+              : `${styles.navItems} ${styles.active}`
+          }
+          onClick={openMenu}
+        >
+          {navItems.map((item) => (
+            <Link key={item.href} href={item.href} className={styles.navItem}>
+              {item.text}
+            </Link>
+          ))}
+        </div>
 
-          <div
-            className={
-              isOpen === false
-                ? styles.navItems
-                : `${styles.navItems} ${styles.active}`
-            }
-            onClick={openMenu}
-          >
-            {navItems.map((item) => (
-              <Link key={item.href} href={item.href} className={styles.navItem}>
-                {item.text}
-              </Link>
-            ))}
-          </div>
-
-          <div className={styles.btnContainer}>
-            <Button
-              href='/'
-              text='Get Started'
-              btnType='noBackgroundWhiteText'
-              arrow
-            />
-          </div>
-          <span
-            className={
-              isOpen === false
-                ? styles.hamburger
-                : `${styles.hamburger} ${styles.active}`
-            }
-            onClick={openMenu}
-          >
-            <span className={styles.whiteBar}></span>
-            <span className={styles.whiteBar}></span>
-            <span className={styles.whiteBar}></span>
-          </span>
-        </nav>
-      </header>
+        <div className={styles.btnContainer}>
+          <Button
+            href='/'
+            text='Get Started'
+            btnType='noBackgroundWhiteText'
+            arrow
+          />
+        </div>
+        <span
+          className={
+            isOpen === false
+              ? styles.hamburger
+              : `${styles.hamburger} ${styles.active}`
+          }
+          onClick={openMenu}
+        >
+          <span className={styles.whiteBar}></span>
+          <span className={styles.whiteBar}></span>
+          <span className={styles.whiteBar}></span>
+        </span>
+      </nav>
+    </header>
   );
 }
