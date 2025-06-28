4164a8d770bb5fc2e525d98b12fb20318478c83d
Chris Ware <c.p.ware@hotmail.com>
2025-06-28 00:07:40 -0700

    project section updated

--- src/components/homePage/ProjectSection/ProjectSection.tsx
+++ src/components/homePage/ProjectSection/ProjectSection.tsx
@@ -1,9 +1,9 @@
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./ProjectSection.module.css";
 import SectionIntro from "@/components/shared/SectionIntro/SectionIntro";
-import { projects } from "../../../../src/lib/data";
+import { projects } from "@/lib/data";
 import Button from "@/components/shared/Button/Button";
-import Image from "next/image";
+import ProjectDetails from "@/components/workPage/ProjectDetails/ProjectDetails";
 
 export default function ProjectSection() {
   return (
@@ -12,7 +12,7 @@ export default function ProjectSection() {
         <div className={styles.content}>
           <div className={styles.top}>
             <div className={styles.sectionHeadingContainer}>
-              <SectionIntro title='our works' dotColor='blueDot' />
+              <SectionIntro title='Featured Project' dotColor='blueDot' />
             </div>
             <h2 className={styles.heading}>
               Check out our{" "}
@@ -21,31 +21,8 @@ export default function ProjectSection() {
             </h2>
           </div>
           <div className={styles.bottom}>
-            {projects.map((x) => (
-              <div className={styles.card} key={x.title}>
-                <div className={styles.cardTop}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      src={x.src}
-                      fill
-                      alt=''
-                      title=''
-                      className={styles.img}
-                    />
-                  </div>
-                  <h3 className={styles.title}>{x.title}</h3>
-                  <span className={styles.category}>Ecommerce</span>
-                  <p className={styles.desc}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Doloribus corporis veritatis ullam ipsa sequi delectus.
-                  </p>
-                </div>
-                <div className={styles.cardBottom}>
-                  <div className={styles.btnContanier}>
-                    <Button href='/' btnType='white' text='Live Site' arrow />
-                  </div>
-                </div>
-              </div>
+            {projects.slice(0, 1).map((project) => (
+              <ProjectDetails key={project.id} project={project} />
             ))}
           </div>
           <div className={styles.btnContanierii}>
