38db6b627ae56eba10e32171b9ec2b8706c22657
Chris Ware <c.p.ware@hotmail.com>
2025-07-09 12:31:03 -0700

    footer icon updated

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -2,6 +2,11 @@ import Arrow from "@/components/icons/Arrow/Arrow";
 import styles from "./Footer.module.css";
 import Logo from "../Logo/Logo";
 import LayoutWrapper from "../LayoutWrapper";
+import Arizona from "@/components/icons/Arizona/Arizona";
+import LinkedIn from "@/components/icons/LinkedIn/LinkedIn";
+import Instagram from "@/components/icons/Instagram/Instagram";
+import Facebook from "@/components/icons/Facebook/Facebook";
+import Link from "next/link";
 
 const data = [
   {
@@ -95,15 +100,30 @@ const data2 = [
   },
 ];
 
+const data3 = [
+  {
+    id: 8,
+    href: "https://www.linkedin.com/feed/",
+    icon: <LinkedIn className={styles.socialIcon} />,
+  },
+  {
+    id: 9,
+    href: "https://www.instagram.com/fontsandfooters/",
+    icon: <Instagram className={styles.socialIcon} />,
+  },
+  {
+    id: 10,
+    href: "https://www.facebook.com/fontsandfooters",
+    icon: <Facebook className={styles.socialIcon} />,
+  },
+];
+
 export default function Footer() {
   return (
     <footer className={styles.container}>
       <LayoutWrapper>
         <div className={styles.logoContainer}>
-          <Logo
-            direction='center'
-            backgroundColor='backgroundColorBlue'
-          />
+          <Logo direction='center' backgroundColor='backgroundColorBlue' />
         </div>
         <div className={styles.top}>
           <div className={styles.left}>
@@ -133,14 +153,25 @@ export default function Footer() {
           </div>
         </div>
         <div className={styles.bottom}>
+          <div className={styles.b1}>
+            <Arizona className={styles.icon} />
+            <p className={styles.iconText}>Based in Phoenix, AZ</p>
+          </div>
           <div className={styles.b2}>
             <div className={styles.data2Container}>
               {data2.map((x) => (
-                <p key={x.id} className={styles.option}>
+                <p key={x.id} className={styles.optionii}>
                   {x.title}
                 </p>
               ))}
             </div>
+          </div>
+          <div className={styles.b3}>
+            {data3.map((x) => (
+              <div key={x.id} className={styles.socialIconContainer}>
+                <Link href={x.href} target="_blank">{x.icon}</Link>
+              </div>
+            ))}
           </div>
         </div>
       </LayoutWrapper>
