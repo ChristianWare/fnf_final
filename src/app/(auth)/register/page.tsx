4c79ac642b2442537717edadd3bc36799a37dcc5
Chris Ware <c.p.ware@hotmail.com>
2025-07-16 22:24:39 -0700

    User Schema

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,12 @@
 import RegisterPageIntro from "@/components/registerPage/RegisterPageIntro";
+import { db } from "@/lib/db";
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  await db.user;
+
   return (
     <main>
       <RegisterPageIntro />
     </main>
-  )
+  );
 }
