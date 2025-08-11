58ce342cabc9889025db0474b06c016256f3e91a
Chris Ware <c.p.ware@hotmail.com>
2025-08-11 15:07:32 -0700

    new hero

--- src/components/homePage/Hero/Hero.tsx
+++ src/components/homePage/Hero/Hero.tsx
@@ -11,8 +11,8 @@ import Modal from "@/components/shared/Modal/Modal";
 import ContactForm from "@/components/shared/ContactForm/ContactForm";
 import { useState } from "react";
 import SectionIntroii from "@/components/shared/SectionIntroii/SectionIntroii";
-// import Image from "next/image";
-// import Img1 from "../../../../public/images/calendarii.png";
+import Image from "next/image";
+import Img1 from "../../../../public/images/newheroii.png";
 
 export default function Hero() {
   const [open, setOpen] = useState(false);
@@ -24,35 +24,40 @@ export default function Hero() {
       </div>
       <LayoutWrapper>
         <div className={styles.content}>
+          <div className={styles.left}>
           <SectionIntroii />
-          <h1 className={styles.heading}>
-            We build <br />{" "}
-            <span className={styles.span}> booking websites </span> <br />
-            that fill calendars
-            {/* , not inboxes. */}
-          </h1>
-          <p className={styles.copy}>
-            Fonts & Footers turns slow DM scheduling into one-click
-            confirmations for salons, spas, rentals, and service brands.
-          </p>
-          <div className={styles.btnContainer}>
-            <FalseButton
-              btnType='white'
-              text='Request free booking audit'
-              onClick={() => setOpen(true)}
-            />
-            <Button
-              href='/contact'
-              btnType='noBackgroundWhiteText'
-              text='Free strategy call'
-              target='_blank'
-              arrow
-            />
+            <h1 className={styles.heading}>
+              We build <br />{" "}
+              <span className={styles.span}> booking websites </span> <br />
+              that fill calendars
+              {/* , not inboxes. */}
+            </h1>
+            <p className={styles.copy}>
+              Fonts & Footers turns slow DM scheduling into one-click
+              confirmations for salons, spas, rentals, and service brands.
+            </p>
+            <div className={styles.btnContainer}>
+              <FalseButton
+                btnType='white'
+                text='Request free booking audit'
+                onClick={() => setOpen(true)}
+              />
+              <Button
+                href='/contact'
+                btnType='noBackgroundWhiteText'
+                text='Free strategy call'
+                target='_blank'
+                arrow
+              />
+            </div>
           </div>
+
           {/* <div className={styles.right}>
             <div className={styles.lottieBox}>
               <Lottie animationData={animationData} className={styles.lottie} />
             </div>
+            </div> */}
+          <div className={styles.right}>
             <div className={styles.imgContainer}>
               <Image
                 src={Img1}
@@ -62,7 +67,7 @@ export default function Hero() {
                 className={styles.img}
               />{" "}
             </div>
-          </div> */}
+          </div>
         </div>
       </LayoutWrapper>
       <Modal isOpen={open} onClose={() => setOpen(false)}>
