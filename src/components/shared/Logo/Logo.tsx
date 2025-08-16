6831b435b658ca5c15603920ecfdd456a9e99702
Chris Ware <c.p.ware@hotmail.com>
2025-08-16 10:54:59 -0700

    logo updated

--- src/components/shared/Logo/Logo.tsx
+++ src/components/shared/Logo/Logo.tsx
@@ -1,6 +1,8 @@
 import styles from "./Logo.module.css";
 import Link from "next/link";
-import Calendar from "@/components/icons/Calendar/Calendar";
+// import Calendar from "@/components/icons/Calendar/Calendar";
+import Image from "next/image";
+import Img1 from "../../../../public/images/smileii.png";
 
 interface Props {
   size?: string;
@@ -20,19 +22,9 @@ const Logo = ({
       href='/'
       className={`${styles.logo} ${styles[size]} ${styles[color]} ${styles[backgroundColor]} ${styles[direction]}`}
     >
-      {/* <div className={styles.imgContainer}>
-        <Image
-          src={Img1}
-          fill
-          alt=''
-          title=''
-          className={styles.img}
-          priority={true}
-        />
-      </div> */}
-      <div className={styles.titleContainer}>
-        <Calendar className={styles.icon} /> Fonts & Footers
-      </div>
+      <Image src={Img1} alt='' title='' className={styles.img} />
+      {/* <Calendar className={styles.icon} /> */}
+      Fonts & Footers
     </Link>
   );
 };
